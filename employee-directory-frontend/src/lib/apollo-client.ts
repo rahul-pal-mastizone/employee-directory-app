5b689f98c24699dd5b141608318956269b633600
Rahul Pal <rahulpal.moderntechno@gmail.com>
2025-07-14 17:17:18 +0530

    chore: connect frontend to deployed backend

--- employee-directory-frontend/src/lib/apollo-client.ts
+++ employee-directory-frontend/src/lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql', // Apollo v3 default path
+  uri: 'https://employee-directory-app-nouq.onrender.com/graphql', // Apollo v3 default path
   cache: new InMemoryCache(),
 });
 
